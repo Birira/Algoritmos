7c4ef5ddf3a6c880585920f0c6a61ced6ad1697b
Vicente <vicente.pereira.ch@gmail.com>
2023-08-25 20:40:11 -0400

    tarea 0 completa

--- tarea0_ver2.c
+++ tarea0_ver2.c
@@ -3,12 +3,11 @@
  #define MAX 1000
  #define MIN 50
 
-void leerArch(){
+int leerArch(float Datos[MAX]){
     char Nombrearchivo[MIN];
     FILE * archivo;
     int n= 0;      /*cantidad de elementos del archivo*/
     int i = 0;
-    float Datos[MAX];
 
     printf("ingresar el nombre del archivo: ");
     scanf("%s", &Nombrearchivo);
@@ -26,10 +25,48 @@ void leerArch(){
             printf("\n %15.3f", Datos[i]);
             i++;
         }
+        return n;
     }
 }
 
+void imprimeLista(float datos[MAX], int n){
+    int i;
+    while (i<n){
+        printf("\n %15.3f", datos[i]);
+        i++;
+    }
+}
+
+void imprimeSuma(float datos[MAX], int n){
+    int i = 0;
+    float sum;
+    while (i<n){
+        sum += datos[i];
+        i++;
+    }
+    printf("\nla suma de los datos es: %.3f",sum);
+}
+
+void imprimeMenor(float datos[MAX], int n){
+    int i = 0;
+    float var = datos[0];
+    while (i<n){
+        if (datos[i] < var){
+            var = datos[i];
+        }
+        i++;
+    }
+    printf("\nel dato menor es: %.3f",var);
+}
+
 int main(){
-    leerArch();
+    float list[MAX];
+    int n;
+
+    n = leerArch(list);
+    imprimeLista(list, n);
+    printf("\n\n");
+    imprimeSuma(list,n);
+    imprimeMenor(list, n);
     return 0;
 }
